cedb6d4242175043ca7f37fa2825a082262efcb3
vlad3k <vlad3k@bk.ru>
2021-09-22 10:06:11 +0300

    fix: delete extra operato

--- rxjs/task-4/src/app/services/token-info.service.ts
+++ rxjs/task-4/src/app/services/token-info.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import getTokenInfo from "../utils/token-info-mock";
-import {BehaviorSubject, interval, Observable, Subject, timer} from "rxjs";
-import {TokenInfo} from "../models/TokenInfo";
-import {map, switchMap} from "rxjs/operators";
+import { BehaviorSubject, interval, Observable, Subject, timer } from "rxjs";
+import { TokenInfo } from "../models/TokenInfo";
+import { map, switchMap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TokenInfoService {
 
   private tokenData$ = new BehaviorSubject<TokenInfo>(undefined);
@@ -22,8 +23,7 @@ export class TokenInfoService {
   constructor() {
     interval(3000)
         .pipe(
-          map(() => this.getTokenInfo()),
-          switchMap(data => data)
+          switchMap((data) => this.getTokenInfo())
         ).subscribe((data: TokenInfo) => this.tokenData$.next(data));
   }
 
